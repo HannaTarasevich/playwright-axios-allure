c5827475cebea0529556e0dc2c23a07c01173c57
Hanna Tarasevich <htarasevich@exadel.com>
2022-02-28 11:53:06 +0300

    Linters adding

--- src/helpers/manualStart.ts
+++ src/helpers/manualStart.ts
@@ -1,12 +1,14 @@
-const { chromium } = require('@playwright/test');
+import { chromium } from '@playwright/test';
 
 (async () => {
   // Make sure to run headed.
   const browser = await chromium.launch({ headless: false });
 
   // Setup context however you like.
-  const context = await browser.newContext({ /* pass any options */ });
-  await context.route('**/*', route => route.continue());
+  const context = await browser.newContext({
+    /* pass any options */
+  });
+  await context.route('**/*', (route) => route.continue());
 
   // Pause the page, and start recording manually.
   const page = await context.newPage();
